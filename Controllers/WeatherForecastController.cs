8a7615f4798789aa9093c5c8c374b2896104de6a
ubhunia <ubhunia@gmail.com>
2023-04-17 15:47:29 +0530

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AngularProject.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","BHUNIA"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
